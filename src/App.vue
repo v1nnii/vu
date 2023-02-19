00242b63869c243ae5ad7d41eb56679d5f45f0a6
v1nnii <zheneskudavid100@mail.ru>
2023-02-19 17:12:43 +0300

    объекты в Vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
             var1: 5,
             var2: 2,
             arr2: [9, 10, 20],
+            obj: { a: 3, b: 8, c: 9 },
+
         }
     }
 }
@@ -19,5 +21,6 @@
     	<p>{{ surname }}</p>
         {{ (var1**2) + var2 }}
         <br>{{ arr2[0] + arr2[1] + arr2[2] }}
+    	<br>{{ obj.a + obj.b + obj.c }}
 	</div>
 </template>
